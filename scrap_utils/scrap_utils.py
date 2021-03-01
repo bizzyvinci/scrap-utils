9a23ff5424e5457c00357f56d6471fd7d3cf0f41
pizzy <horlasehinde@gmail.com>
2021-03-01 21:00:13 +0100

    Improvement to dump_json and little for load_json (mainly doc) and change var data to obj

--- scrap_utils/scrap_utils.py
+++ scrap_utils/scrap_utils.py
@@ -25,7 +25,7 @@ def load_json(filepath, encoding=None, errors=None, parse_float=None,
     """
     Load json from file.
 
-    Open file with open() function and load data with json.load()
+    Open file with `open()` function and load data with `json.load()`.
 
     Parameters
     ----------
@@ -53,6 +53,12 @@ def load_json(filepath, encoding=None, errors=None, parse_float=None,
         It will be called with one of the following strings:
         `'-Infinity'`, `'Infinity'`, `'NaN'`.
 
+    Returns
+    -------
+    obj: python object
+        A python object from the json using this `conversion table
+        <https://docs.python.org/3/library/json.html#json-to-py-table>`_.
+
     References
     ----------
     .. [1] `open() documentation:
@@ -61,21 +67,67 @@ def load_json(filepath, encoding=None, errors=None, parse_float=None,
         <https://docs.python.org/3/library/json.html#json.load>`_
     """
     with open(filepath, 'r', encoding=encoding, errors=errors) as file:
-        data = json.load(file, parse_float=parse_float, parse_int=parse_int,
-                         parse_constant=parse_constant)
-    return data
+        obj = json.load(file, parse_float=parse_float, parse_int=parse_int,
+                        parse_constant=parse_constant)
+    return obj
 
 
-def dump_json(data, filepath, encoding=None, errors=None, indent=4,
-        skipkeys=False, ensure_ascii=False, separators=None, sort_keys=False):
-    """Dump json data into filepath."""
+def dump_json(obj, filepath, encoding=None, errors=None, indent=4,
+              skipkeys=False, ensure_ascii=False, separators=None,
+              sort_keys=False):
+    """
+    Dump json data into filepath.
+
+    Open file with `open()` function and dump json with `json.dump()`.
+
+    Parameters
+    ----------
+    obj: python object
+        A python object to convert to json using this `conversion table
+        <https://docs.python.org/3/library/json.html#py-to-json-table>`_.
+    filepath: string
+        filepath to save the json.
+    encoding: string
+        The name of the encoding used to decode or encode the file.
+        Default is whatever `locale.getpreferredencoding()` returns.
+        `List of standand encoding
+        <https://docs.python.org/3/library/codecs.html#standard-encodings>`_.
+    errors: string, optional
+        Specifies how encoding and decoding error should be handled
+        The standard names include [1]_: `strict`, `ignore`, `replace`,
+        `surrogateescape`, `xmlcharrefreplace`, `backslashreplace`,
+        `namereplace`.
+    indent: int, optional
+        A positive integer indicates the number of spaces to indent levels.
+    skipkeys: bool, optional
+        If `True` (default is `False`), dict keys that are not of a basic type
+        (str, int, float, bool, None) will be skipped
+        instead of raising a TypeError.
+    ensure_ascii: bool, optional
+        If ensure_ascii is `True` (the default), the output is
+        guaranteed to have all incoming non-ASCII characters escaped.
+        If ensure_ascii is `False`, these characters will be output as-is.
+    seperators: (item_separator, key_separator) tuple, optional
+        default is `(', ', ': ')`.
+        To eliminate space and make json compact, use `(',', ':')`
+        and set `indent` to `None`.
+    sort_keys: bool, optional
+        If sort_keys is `True` (default: `False`),
+        then the output of dictionaries will be sorted by key.
+
+    References
+    ----------
+    .. [1] `open() documentation:
+        <https://docs.python.org/3/library/functions.html#open>`_
+    .. [2] `json.dump() documentation
+        <https://docs.python.org/3/library/json.html#json.dump>`_
+    """
     with open(filepath, 'w', encoding=encoding, errors=errors) as file:
         json.dump(
-            data, file, indent=indent, skipkeys=skipkeys,
+            obj, file, indent=indent, skipkeys=skipkeys,
             ensure_ascii=ensure_ascii, separators=separators,
             sort_keys=sort_keys
         )
-    print("Json dumped to", filepath)
 
 
 def to_csv(dataset, filepath, mode="a", encoding=None, errors=None, newline='',
