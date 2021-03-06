ec6779afd246c8981a30086bda003ca288872ee9
pizzy <horlasehinde@gmail.com>
2021-03-06 09:18:05 +0100

    Split scrap_utils.py to file.py and requests.py

--- scrap_utils/__init__.py
+++ scrap_utils/__init__.py
@@ -1,1 +1,24 @@
-from .scrap_utils import *
+"""
+============
+Scrap Utils
+============
+This module provides some functions that might save you from repetition
+in scraping projects.
+
+Functions
+---------
++-------------------+-----------------------------------------------+
+| load_json         | Load json from file                           |
++-------------------+-----------------------------------------------+
+| dump_json         | Dump json into filepath                       |
++-------------------+-----------------------------------------------+
+| to_csv            | Save dataset to csv file                      |
++-------------------+-----------------------------------------------+
+| requests_get      | Send a GET request with requests library      |
++-------------------+-----------------------------------------------+
+| requests_post     | Send a POST request with requests library     |
++-------------------+-----------------------------------------------+
+"""
+
+from .file import *
+from .requests import *
