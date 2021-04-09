d294baea31f0d33eaae8117ba1b0a59a400d0a8b
pizzy <horlasehinde@gmail.com>
2021-04-09 17:24:22 +0100

    Create read_csv and enable dict reader/writer :heart:

--- scrap_utils/file.py
+++ scrap_utils/file.py
@@ -1,6 +1,6 @@
 import csv
 import json
-
+import logging
 
 def load_json(filepath, encoding=None, errors=None, parse_float=None,
               parse_int=None, parse_constant=None):
@@ -116,23 +116,31 @@ def dump_json(obj, filepath, encoding=None, errors=None, indent=4,
         )
 
 
-def to_csv(dataset, filepath, mode="a", encoding=None, errors=None, newline='',
-           header=True, dialect='excel', **fmtparams):
+def to_csv(dataset, filepath, dictionary=False, fieldnames=[], header=True,
+           mode="a", encoding=None, errors=None, newline='', dialect='excel',
+           **kwargs):
     """
     Save dataset to csv file.
 
-    Open file with `open()` function and write with `csvwriter.writerows()`.
+    Open file with `open()` function and write with 
+    `csv.writer.writerows()` if dictionary is False or
+    `csv.DictWriter.writerows()` if dictionary is True.
 
     Parameters
     ----------
-    dataset: iterable[ of iterables]
+    dataset: iterable of iterables or dictionaries
         The dataset to save [3]_.
-        If dataset is 1D e.g list, tuple, set,
-        the csv would have a column.
-        If dataset is 2D e.g list of list or list of tuple,
+        2D e.g list of list or list of tuple or list of dictionary,
         the inner iterables would be row.
     filepath: str
         filepath to save dataset.
+    dictionary: bool, default False
+        If DictWriter should be used
+        (when it is an iterable of dictionaries).
+    fieldnames: iterable, default []
+        An iterable of keys that identify the order in which values in the
+        dictionary passed are written to the csv file.
+        This should be specified if dictionary is True.
     mode: str, default 'a'
         Mode in which file is opened.
         Default is `'a'` which appends at the end of the file if it exists.
@@ -151,12 +159,12 @@ def to_csv(dataset, filepath, mode="a", encoding=None, errors=None, newline='',
         Default is `''` [1]_.
     header: bool, default True
         Should header or first row be included in the file?
-        Default is True, include header or first row,
+        Default is True, include header or first row.
     dialect: string or subclass of `csv.Dialect
     <https://docs.python.org/3/library/csv.html#csv.Dialect>`_, optional
         Default is `'excel'`.
-    **fmtparams: Dialects and formatting parameters, optional
-        For full details see `Dialects and formatting parameters
+    **kwargs: Other parameters for csv.writer or csv.DictWriter, optional
+        For more details see reference and `Dialects and formatting parameters
         <https://docs.python.org/3/library/csv.html#csv-fmt-params>`
 
     Returns
@@ -167,19 +175,103 @@ def to_csv(dataset, filepath, mode="a", encoding=None, errors=None, newline='',
     ----------
     .. [1] `open() documentation
         <https://docs.python.org/3/library/functions.html#open>`_
-    .. [2] `csvwriter documentation
+    .. [2] `csv.writer documentation
         <https://docs.python.org/3/library/csv.html#csv.writer>`_
-    .. [3] `writer() method documentation
+    .. [3] `csv.DictWriter documentation
+        <https://docs.python.org/3/library/csv.html#csv.DictWriter>`_
+    .. [4] `writerows() method documentation
         <https://docs.python.org/3/library/csv.html#csv.csvwriter.writerows>`_
     """
     with open(filepath, mode=mode, encoding=encoding, errors=errors,
               newline=newline) as csvfile:
-        writer = csv.writer(csvfile, dialect=dialect, **fmtparams)
-        if header:
-            writer.writerows(dataset)
+        if dictionary:
+            if fieldnames:
+                writer = csv.DictWriter(csvfile, fieldnames=fieldnames, **kwargs)
+                if header:
+                    writer.writeheader()
+                writer.writerows(dataset)
+            else:
+                logging.warning(
+                    "fieldnames is not specified/empty. "
+                    "Therefore, nothing might be saved in csv file."
+                )
         else:
-            writer.writerows(dataset[1:])
+            writer = csv.writer(csvfile, dialect=dialect, **kwargs)
+            if header:
+                writer.writerows(dataset)
+            else:
+                writer.writerows(dataset[1:])
 
 
-def read_csv():
-    pass
+
+def read_csv(filepath, dictionary=False, fieldnames=None, header=True, mode="r",
+             encoding=None, errors=None, newline='', dialect='excel', **kwargs):
+    """
+    Read dataset from csv file.
+
+    Open file with `open()` function and write with 
+    `csv.reader` when dictionary is False or
+    `csv.DictReader` when dictionary is True.
+
+    Parameters
+    ----------
+    filepath: str
+        filepath to save dataset.
+    dictionary: bool, default False
+        If DictReader should be used and 
+        hence return iterable of dictionaries.
+    fieldnames: iterable, default None
+        An iterable of the keys in the dataset's dictionaries.
+        If fieldnames is omitted, the values in the first row of
+        csv file will be used as the fieldnames.
+    header: bool, default True
+        Should header or first row of the file be included in the dataset?
+        Default is True, include header or first row.
+    mode: str, default 'r'
+        Mode in which file is opened.
+        Default is `'r'`.
+    encoding: str, optional
+        The name of the encoding used to decode or encode the file.
+        Default is whatever `locale.getpreferredencoding()` returns.
+        `List of standand encoding
+        <https://docs.python.org/3/library/codecs.html#standard-encodings>`_.
+    errors: str, default None
+        Specifies how encoding and decoding error should be handled
+        The standard names include [1]_: `strict`, `ignore`, `replace`,
+        `surrogateescape`, `xmlcharrefreplace`, `backslashreplace`,
+        `namereplace`.
+    newline: str, optional
+        Default is `''` [1]_.
+    dialect: string or subclass of `csv.Dialect
+    <https://docs.python.org/3/library/csv.html#csv.Dialect>`_, optional
+        Default is `'excel'`.
+    **kwargs: other parameters for csv.reader or csv.Dict.Reader, optional
+        For more details see [2]_, [3]_ and `Dialects and formatting parameters
+        <https://docs.python.org/3/library/csv.html#csv-fmt-params>`
+
+    Returns
+    -------
+    dataset: list of lists or dictionary
+        The dataset read from the file.
+
+    References
+    ----------
+    .. [1] `open() documentation
+        <https://docs.python.org/3/library/functions.html#open>`_
+    .. [2] `csv.reader documentation
+        <https://docs.python.org/3/library/csv.html#csv.reader>`_
+    .. [3] `csv.DictReader method documentation
+        <https://docs.python.org/3/library/csv.html#csv.DictReader>`_
+    """
+    with open(filepath, mode=mode, encoding=encoding, errors=errors,
+              newline=newline) as csvfile:
+        if dictionary:
+            reader = csv.DictReader(csvfile, fieldnames=fieldnames,
+                dialect=dialect, **kwargs)
+            return list(reader)
+        else:
+            reader = csv.reader(csvfile, dialect=dialect, **kwargs)
+            if header:
+                return list(reader)
+            else:
+                return list(reader)[1:]
